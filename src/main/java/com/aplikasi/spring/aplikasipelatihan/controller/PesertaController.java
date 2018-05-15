9a7401a9a91ed9aba6e62d56af9f12c2fbec0aa7
Sultan Ul Haq Sanggabuana <saaanq@gmail.com>
2018-05-15 16:29:52 +0700

    video-13

---
+++ src/main/java/com/aplikasi/spring/aplikasipelatihan/controller/PesertaController.java
@@ -0,0 +1,52 @@
+package com.aplikasi.spring.aplikasipelatihan.controller;
+
+import com.aplikasi.spring.aplikasipelatihan.dao.PesertaDao;
+import com.aplikasi.spring.aplikasipelatihan.entity.Peserta;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PesertaController {
+    @Autowired
+    private PesertaDao pd;
+    
+    @RequestMapping(value = "/peserta", method = RequestMethod.GET)
+    public Page<Peserta> cariPeserta(Pageable page){
+        return pd.findAll(page);
+    }
+    
+    @RequestMapping(value = "/peserta", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void insertPesertaBaru(@RequestBody Peserta p){
+        pd.save(p);
+    }
+    
+    @RequestMapping(value = "/peserta/{id}", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.OK)
+    public void updatePeserta(@PathVariable("id") String id, @RequestBody Peserta p){
+        p.setId(id);
+        pd.save(p);
+    }
+    
+    @RequestMapping(value="/peserta/{id}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Peserta> cariPesertaById(@PathVariable("id") String id){
+        return pd.findById(id);
+    }
+    
+    @RequestMapping(value = "/peserta/{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.OK)
+    public void hapusPeserta(@PathVariable("id") String id){
+        pd.deleteById(id);
+    }
+}
